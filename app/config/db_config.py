6be8802a0f63c9d892a14c1ac31f6d2ef6800f34
Fernando <fernandoferfernandez1233@gmail.com>
2026-05-22 14:00:13 -0500

    Configurar base de datos para producción

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -1,6 +1,9 @@
+import os
 import psycopg2
 
 def get_connection():
-    return psycopg2.connect(
+    db_url = os.getenv(
+        "DATABASE_URL",
         "postgresql://neondb_owner:npg_1ugjFDsEAW0k@ep-rough-resonance-ambvbrlf-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
     )
+    return psycopg2.connect(db_url)

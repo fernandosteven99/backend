668352b65bef80f8e43b6cff7c3b04526aae1f38
fernandosteven99 <fernandoferfernandez1233@gmail.com>
2026-05-22 20:36:21 -0500

    Update notification_controller.py

--- app/controllers/notification_controller.py
+++ app/controllers/notification_controller.py
@@ -1,12 +1,19 @@
-from app.models.notification_model import *
+from app.models.notification_model import get_notifications, get_notifications_by_user, create_notification, mark_as_seen, delete_notification
 
 def list_notifications():
     return get_notifications()
 
-def create_new_notification(data):
-    create_notification(data.student_id, data.message)
-    return {"message": "Notificación creada"}
+def list_notifications_by_user(user_id: int):
+    return get_notifications_by_user(user_id)
 
-def delete_existing_notification(id):
+def create_new_notification(user_id: int, sender_id: int, message: str):
+    create_notification(user_id, sender_id, message)
+    return {"message": "Notificación enviada"}
+
+def mark_notification_seen(notification_id: int):
+    mark_as_seen(notification_id)
+    return {"message": "Notificación marcada como leída"}
+
+def delete_existing_notification(id: int):
     delete_notification(id)
     return {"message": "Notificación eliminada"}
